771c12ab1c799bb4b5c17b7fed730d0c1b00872f
Shajedul Islam <shajedul.islam@naztech.us.com>
2019-03-23 14:39:34 +0600

    testcase for label and abstract fetching and another testcase for all data fetching added

--- src/main/java/io/naztech/fetchProgrammingLanguagesFromDBPedia/FetchDataFromDBPedia.java
+++ src/main/java/io/naztech/fetchProgrammingLanguagesFromDBPedia/FetchDataFromDBPedia.java
@@ -1,20 +1,24 @@
 package io.naztech.fetchProgrammingLanguagesFromDBPedia;
 
-import org.apache.jena.query.ParameterizedSparqlString;
-import org.apache.jena.query.QueryExecution;
-import org.apache.jena.query.QueryExecutionFactory;
+//import org.apache.jena.query.ParameterizedSparqlString;
+//import org.apache.jena.query.QueryExecution;
+//import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.ResultSet;
 //import org.apache.jena.query.ResultSetFormatter;
+import org.apache.jena.sparql.engine.http.QueryEngineHTTP;
 
 public class FetchDataFromDBPedia {
 	
+	private QueryEngineHTTP qe;
+
 	public ResultSet fetchdata(String query)
 	{
-		ParameterizedSparqlString qs = new ParameterizedSparqlString(query);
+		//ParameterizedSparqlString qs = new ParameterizedSparqlString(query);
 
-        QueryExecution exec = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", qs.asQuery());
-
-        ResultSet results = exec.execSelect();
+        //QueryExecution exec = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", qs.asQuery());
+        
+        qe = new QueryEngineHTTP("http://dbpedia.org/sparql", query);
+        ResultSet results = qe.execSelect();
 
         return results;
 	}
--- src/test/java/io/naztech/fetchProgrammingLanguagesFromDBPedia/testDataFetching.java
+++ src/test/java/io/naztech/fetchProgrammingLanguagesFromDBPedia/testDataFetching.java
@@ -1,5 +1,6 @@
 package io.naztech.fetchProgrammingLanguagesFromDBPedia;
 
+import org.apache.jena.query.QuerySolution;
 //import static org.junit.Assert.*;
 import org.apache.jena.query.ResultSet;
 //import org.apache.jena.query.ResultSetFormatter;
@@ -12,25 +13,153 @@ public class testDataFetching {
 	@Test
 	public void testLabelFetching() {
 		
-		String q = "prefix rdf:    <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
-                 + "prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#>\n"
-                 + "PREFIX dbo:     <http://dbpedia.org/ontology/>\n"
-                 + "SELECT ?pl ?label \n" 
-                 + "WHERE { \n"  
-                
-                 			+"?pl rdfs:label ?label .\n" 
-                 			+"?pl rdf:type dbo:ProgrammingLanguage .\n" 
-                            +"FILTER (LANG(?label)='en')"
- 
-                 +		 "}";
+		String q = 	"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \n"+
+					"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"+
+					"PREFIX dbo: <http://dbpedia.org/ontology/>\n"+
+					
+					"SELECT ?pl ?pl_label \n" +
+					
+					
+					"WHERE 	{\n" +
+					
+					"	   	?pl rdf:type dbo:ProgrammingLanguage .\n" + 
+						
+					"		?pl rdfs:label ?pl_label .\n" + 
+						
+					"		FILTER ( LANG ( ?pl_label ) = 'en' ) .\n" + 
+					
+					"	   	}"+
+					
+					"		GROUP BY ?pl ?pl_label";
 		
 		
-		ResultSet results = fetch.fetchdata(q);
+		ResultSet results = fetch.fetchdata(q);	
+		
+		int count = 0;
 		
 		while (results.hasNext()) 
 		{
-            System.out.println(results.next().get("label").toString()+"");  
+			QuerySolution qs =  results.next();
+            System.out.println("NAME-->\n"+qs.get("pl_label").toString()+"\n"); 
+            System.out.println("\n\n");
+           
+            count++;
         }
+		
+		System.out.println(count);
+		
+	    //ResultSetFormatter.out(results);
+		
+	}
+	
+	
+	@Test
+	public void testLabelandAbstractFetching() {
+		
+		String q = 	"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \n"+
+					"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"+
+					"PREFIX dbo: <http://dbpedia.org/ontology/>\n"+
+					
+					"SELECT ?pl ?pl_label ?abstract\n" +
+					
+					
+					"WHERE 	{\n" +
+					
+					"	   	?pl rdf:type dbo:ProgrammingLanguage .\n" + 
+					
+					"      	?pl dbo:abstract ?abstract .\n" + 
+						
+					"		FILTER ( LANG ( ?abstract ) = 'en' ) .\n" + 
+						
+					"		?pl rdfs:label ?pl_label .\n" + 
+						
+					"		FILTER ( LANG ( ?pl_label ) = 'en' ) .\n" + 
+					
+					"	   	}"+
+					
+					"		";
+		
+		
+		ResultSet results = fetch.fetchdata(q);	
+		
+		int count = 0;
+		
+		while (results.hasNext()) 
+		{
+			QuerySolution qs =  results.next();
+            System.out.println("NAME-->\n"+qs.get("pl_label").toString()+"\n"); 
+            System.out.println("ABSTRACT-->\n"+qs.get("abstract").toString()+"\n"); 
+            System.out.println("\n\n");
+           
+            count++;
+        }
+		
+		System.out.println(count);
+		
+	    //ResultSetFormatter.out(results);
+		
+	}
+	
+	
+	
+	@Test
+	public void testAllDataFetching() {
+		
+		String q = 	"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \n"+
+					"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"+
+					"PREFIX dbo: <http://dbpedia.org/ontology/>\n"+
+					
+					"SELECT ?pl ?pl_label \n" +
+					"( Group_concat ( ?_influenced_label; separator= \", \")   AS ?influenced ) \n" + 
+					"( Group_concat ( ?_influencedBy_label; separator= \", \") AS ?influencedBy ) \n" + 
+					"?abstract \n" +
+					
+					"WHERE 	{\n" +
+					
+					"	   	?pl rdf:type dbo:ProgrammingLanguage .\n" + 
+					
+					"      	?pl dbo:abstract ?abstract .\n" + 
+						
+					"		FILTER ( LANG ( ?abstract ) = 'en' ) .\n" + 
+						
+					"		?pl rdfs:label ?pl_label .\n" + 
+						
+					"		FILTER ( LANG ( ?pl_label ) = 'en' ) .\n" + 
+						
+					"		?pl dbo:influenced ?_influenced .\n" + 
+						
+					"		?_influenced rdfs:label ?_influenced_label .\n" + 
+						
+					"		FILTER ( LANG ( ?_influenced_label ) = 'en' ) .\n" + 
+						
+					"		?pl dbo:influencedBy  ?_influencedBy .\n" + 
+						
+					"		?_influencedBy  rdfs:label ?_influencedBy_label .\n" + 
+						
+					"		FILTER ( LANG ( ?_influencedBy_label ) = 'en' ) .\n" + 
+					
+					"	   	}"+
+					
+					"		GROUP BY ?pl ?pl_label ?abstract";
+		
+		
+		ResultSet results = fetch.fetchdata(q);	
+		
+		int count = 0;
+		
+		while (results.hasNext()) 
+		{
+			QuerySolution qs =  results.next();
+            System.out.println("NAME-->\n"+qs.get("pl_label").toString()+"\n"); 
+            System.out.println("INFLUENCED-->\n"+qs.get("influenced").toString()+"\n"); 
+            System.out.println("INFLUENCED BY-->\n"+qs.get("influencedBy").toString()+"\n"); 
+            System.out.println("ABSTRACT-->\n"+qs.get("abstract").toString()+"\n"); 
+            System.out.println("\n\n");
+           
+            count++;
+        }
+		
+		System.out.println(count);
 		
 	    //ResultSetFormatter.out(results);
 		
